3b01370b07a4096b4cb816629a6045b2d75d51ff
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-10 06:41:53 +0000

    [Add]homes_controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5f5fd16dcba54ced9972758d06fea104.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
