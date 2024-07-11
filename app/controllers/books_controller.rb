c26706414f91dc3eea86b5536818dc684686e1a4
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-11 00:12:29 +0000

    [Add]保存機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+	  @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   def show
   end
 
   def edit
+  end
+
+private
+  def book_params
+  params.require(:book).permit(:title, :body)
   end
 
 end
